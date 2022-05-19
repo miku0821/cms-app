cf0d693ea50c01719ad09434a135bba4da52b88f
Miku <miku.2113.love@gmail.com>
2022-05-20 01:24:43 +0900

    make it better

--- resources/views/includes/home-nav.blade.php
+++ resources/views/includes/home-nav.blade.php
@@ -17,9 +17,9 @@
             <li class="nav-item">
               <a class="nav-link" href="/login">Login</a>
             </li>   
-            <li class="nav-item">
+            {{-- <li class="nav-item">
               <a class="nav-link" href="/register">Register</a>
-            </li>   
+            </li>    --}}
           @endif
         </ul>
       </div>
